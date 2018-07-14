cc26931cecb7fea7a68faa11fbf8c1baeae7e8b1
Andrey Alekseev <home@Andreys-MacBook-Pro.local>
2018-07-14 23:37:02 +0200

    added 2 pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Contact = () => (
+  <div>
+    <h1>Contact Us</h1>
+    <p>Welcome to page Contact Us</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default Contact
