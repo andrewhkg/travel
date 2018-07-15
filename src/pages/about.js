04dab3cb30a9fa86c1ef43379f24631172d23b74
Andrey Alekseev <home@Andreys-MacBook-Pro.local>
2018-07-16 01:52:09 +0200

    added styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,18 @@ const About = () => (
   <div>
     <h1>About Us</h1>
     <p>Welcome to page About Us!</p>
+    <p>
+    Travel the Text offers study trips to Israel, Jordan, Turkey, Greece, and Italy. Most trips are 14 days, and all are designed for those who dream visiting the land of the Bible as serious students of the Text. Each study program offers a unique curriculum developed and taught by Geoff Carroll. All study is on-site, as we seek to engage the land from the perspectives of the original audience.
+    </p>
+    <p>
+    Travel the Text offers study trips to Israel, Jordan, Turkey, Greece, and Italy. Most trips are 14 days, and all are designed for those who dream visiting the land of the Bible as serious students of the Text. Each study program offers a unique curriculum developed and taught by Geoff Carroll. All study is on-site, as we seek to engage the land from the perspectives of the original audience.
+    </p>
+    <p>
+    Travel the Text offers study trips to Israel, Jordan, Turkey, Greece, and Italy. Most trips are 14 days, and all are designed for those who dream visiting the land of the Bible as serious students of the Text. Each study program offers a unique curriculum developed and taught by Geoff Carroll. All study is on-site, as we seek to engage the land from the perspectives of the original audience.
+    </p>
+    <p>
+    Travel the Text offers study trips to Israel, Jordan, Turkey, Greece, and Italy. Most trips are 14 days, and all are designed for those who dream visiting the land of the Bible as serious students of the Text. Each study program offers a unique curriculum developed and taught by Geoff Carroll. All study is on-site, as we seek to engage the land from the perspectives of the original audience.
+    </p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
