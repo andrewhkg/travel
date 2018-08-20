b2e8325032a23c2a14456eff681f13b197dc019d
Andrey Alekseev <aandrewaleks@gmail.com>
2018-08-20 20:35:42 +0300

    modified content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,14 @@ import Link from 'gatsby-link'
 const Contact = () => (
   <div>
     <h1>Contact Us</h1>
-    <p>Welcome to page Contact Us</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Address: office 403, Tai Sang Bank Building</p>
+    <p>130-132 Des Vouex Road Central</p>
+    <p>Hong Kong</p>
+
+    <p>Phone: (+852) 81777286</p>
+    <p>Email: info@aasia.hk</p>
+    
+    {/* <Link to="/">Go back to the homepage</Link> */}
   </div>
 )
 
