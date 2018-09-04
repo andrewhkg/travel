0e070700ffc2ad7b82b6841392a77ae91d2f6400
Andrey Alekseev <aandrewaleks@gmail.com>
2018-09-04 03:08:45 +0300

    added react social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import logo from "./logo.png"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { SocialIcon } from 'react-social-icons';
 
 
 const Footer = ({ siteTitle }) => (
@@ -13,10 +13,13 @@ const Footer = ({ siteTitle }) => (
             <strong>AAsia Travel</strong> <br />
             The website content is licensed.
             </p>
-            <FontAwesomeIcon icon="stroopwafel" />
-            
+            <SocialIcon url="http://twitter.com/jaketrent" network="twitter" style={{ height: 30, width: 30, marginRight: '20px' }} />
+            <SocialIcon url="http://facebook.com/Aasia-Travel-524704284208335" network="facebook" style={{ height: 30, width: 30, marginRight: '20px' }} />
+            <SocialIcon url="http://instagram.com/jaketrent" network="instagram"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
+            <SocialIcon url="http://vk.com/jaketrent" network="vk"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
+
             </div>
-        </div>
+      
         </footer>
     </div>
 )
