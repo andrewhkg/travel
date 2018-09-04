b23a5abefa4a059bd9e976c18fc06ffaeb58debf
Andrey Alekseev <aandrewaleks@gmail.com>
2018-09-04 03:10:16 +0300

    upd

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,10 +13,10 @@ const Footer = ({ siteTitle }) => (
             <strong>AAsia Travel</strong> <br />
             The website content is licensed.
             </p>
-            <SocialIcon url="http://twitter.com/jaketrent" network="twitter" style={{ height: 30, width: 30, marginRight: '20px' }} />
+            <SocialIcon url="http://twitter.com/" network="twitter" style={{ height: 30, width: 30, marginRight: '20px' }} />
             <SocialIcon url="http://facebook.com/Aasia-Travel-524704284208335" network="facebook" style={{ height: 30, width: 30, marginRight: '20px' }} />
-            <SocialIcon url="http://instagram.com/jaketrent" network="instagram"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
-            <SocialIcon url="http://vk.com/jaketrent" network="vk"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
+            <SocialIcon url="http://instagram.com/" network="instagram"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
+            <SocialIcon url="http://vk.com/" network="vk"  style={{ height: 30, width: 30, marginRight: '20px' }}/>           
 
             </div>
       
